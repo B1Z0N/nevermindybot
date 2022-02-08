495c04f589caaef61ab41f58a70ed8a7b8da98d8
B1Z0N <kolausf@gmail.com>
2022-02-08 13:27:18 +0200

    Bot setup.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # nevermindy
 Spaced repetition todos 
+
+# Setup
+
+Fulfill the `appsettings.exmpl.json` file and rename it to `appsettings.prod.json`.
---
+++ src/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using System.Threading.Tasks;
+
+using Telegram.Bot;
+
+
+public static class Program
+{
+    const string accessToken = "ACCESS_TOKEN";
+    static IConfiguration conf = new ConfigurationBuilder().AddJsonFile("appsettings.json", false, true).Build();
+
+    public static async Task Main()
+    {
+        var botClient = new TelegramBotClient(conf[accessToken]);
+        
+        var me = await botClient.GetMeAsync();
+        Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
+    }
+}
+
