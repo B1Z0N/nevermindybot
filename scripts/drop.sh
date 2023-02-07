a4cc726bccf488748c9a64257a1af57d0e6d68f6
Mykola Fedurko <kolausf@gmail.com>
2023-02-07 19:26:28 +0200

    Extended script customization to different hosts and ports.

--- scripts/drop.sh
+++ scripts/drop.sh
@@ -3,4 +3,17 @@
 script_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$script_path"
 
-psql postgres -f  drop.sql
+PSQL_HOST="0.0.0.0"
+PSQL_PORT="5432"
+
+echo    "[!nevermindy] Connecting to your database, please specify a host(default=$PSQL_HOST)"
+echo -n "[nevermindy] Enter the host(press enter for default): "
+read -r host
+host=${host:-$PSQL_HOST}
+
+echo    "[nevermindy] Connecting to your database, please specify a port(default=\"5432\")"
+echo -n "[nevermindy] Enter the port(press enter for default): "
+read -r port
+port=${port:-$PSQL_PORT}
+
+psql -h $host -p $port -U postgres -f  drop.sql
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,16 +6,28 @@ cd "$script_path"
 FIBONACCI_TIMESPAN_FIRST="1.00:00:00"
 FIBONACCI_TIMESPAN_SECOND="3.00:00:00"
 POLLING_INTERVAL_TIMESPAN="00:20:00"
+PSQL_HOST="0.0.0.0"
+PSQL_PORT="5432"
 
-echo    "[nevermindy] Creating the database nevermindydb for user nevermindy."
+echo    "[!nevermindy] Connecting to your database, please specify a host(default=$PSQL_HOST)"
+echo -n "[nevermindy] Enter the host(press enter for default): "
+read -r host
+host=${host:-$PSQL_HOST}
+
+echo    "[nevermindy] Connecting to your database, please specify a port(default=\"5432\")"
+echo -n "[nevermindy] Enter the port(press enter for default): "
+read -r port
+port=${port:-$PSQL_PORT}
+
+echo    "[!nevermindy] Creating the database nevermindydb for user nevermindy."
 
 echo -n "[nevermindy] Please enter a password: "
 read -r password
 
-echo -n "[nevermindy] Please enter bot token(from BotFather): "
+echo -n "[!nevermindy] Please enter bot token(from BotFather): "
 read -r token
 
-echo    "[nevermindy] Choosing timespan(TimeSpan.ToString()) for first fibonacci element(default=$FIBONACCI_TIMESPAN_FIRST)."
+echo    "[!nevermindy] Choosing timespan(TimeSpan.ToString()) for first fibonacci element(default=$FIBONACCI_TIMESPAN_FIRST)."
 echo -n "[nevermindy] Enter the timespan(press enter for default): "
 
 read -r first_fib_timespan
@@ -33,8 +45,8 @@ echo -n "[nevermindy] Enter the timespan(press enter for default): "
 read -r polling_interval_timespan
 polling_interval_timespan=${polling_interval_timespan:-$POLLING_INTERVAL_TIMESPAN}
 
-psql postgres -f setup_db.sql -v password="'${password}'"
-psql nevermindydb -f setup_schema.sql
+psql -h $host -p $port -U postgres -f setup_db.sql -v password="'${password}'"
+psql -h $host -p $port -U postgres -d nevermindydb -f setup_schema.sql 
 connection_string="User ID=nevermindy;Password=${password};Host=localhost;Port=5432;Database=nevermindydb;"
 
 res=$(sed -e "s/YOUR_BOT_ACCESS_TOKEN/$token/g" "../appsettings.exmpl.json" \
